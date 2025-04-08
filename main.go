4f0546adc86a149183b44efba3a28a86bb83ad82
unknown <ezekuiel100@gmail.com>
2025-04-07 22:08:36 -0300

    fix readChar

--- main.go
+++ main.go
@@ -21,13 +21,18 @@ func main() {
 
 	lexer.readChar()
 
-	for lexer.readPosition < len(lexer.input) {
-		lexer.createToken()
+	for {
+		tok := lexer.createToken()
+		fmt.Println(tok)
+
+		if tok.token == "EOF" {
+			break
+		}
 	}
 
 }
 
-func (l *Lexer) createToken() {
+func (l *Lexer) createToken() Token {
 	var tok Token
 	l.skipWhiteSpace()
 
@@ -41,43 +46,47 @@ func (l *Lexer) createToken() {
 	default:
 		if l.isLetter(l.ch) {
 			start := l.position
-			for l.isLetter(l.peekChar()) {
+			for l.isLetter(l.ch) {
 				l.readChar()
 			}
-			tok = Token{token: "IDENT", literal: l.input[start:l.readPosition]}
+			tok = Token{token: "IDENT", literal: l.input[start:l.position]}
+			return tok
+		} else {
+			tok = Token{token: "ILLEGAL", literal: string(l.ch)}
+
 		}
 
 	}
 
 	l.readChar()
-	fmt.Println(tok)
+	return tok
 }
 
 func (l *Lexer) readChar() {
 	if l.readPosition >= len(l.input) {
 		l.ch = 0
 	} else {
-		l.ch = l.input[l.position]
-		l.position = l.readPosition
-		l.readPosition++
+		l.ch = l.input[l.readPosition]
 	}
 
+	l.position = l.readPosition
+	l.readPosition++
 }
 
 func (Lexer) isLetter(ch byte) bool {
 	return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || ch == '_'
 }
 
-func (l *Lexer) peekChar() byte {
-	if l.readPosition > len(l.input) {
-		return 0
-	} else {
-		return l.input[l.readPosition]
-	}
-}
+// func (l *Lexer) peekChar() byte {
+// 	if l.readPosition >= len(l.input) {
+// 		return 0
+// 	} else {
+// 		return l.input[l.readPosition]
+// 	}
+// }
 
 func (l *Lexer) skipWhiteSpace() {
-	if l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
 		l.readChar()
 	}
 }
